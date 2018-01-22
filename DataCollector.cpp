90946cd7ce191a4890bf34c6a2319a55e1e26535
Ben Plumley <benplumley@gmail.com>
2018-01-22 22:28:46 +0000

    structure

--- DataCollector.cpp
+++ DataCollector.cpp
@@ -6,12 +6,18 @@ using namespace std;
 const LPTSTR dllPath = "C:\\Program Files (x86)\\NaturalPoint\\TrackIR5";
 
 class DataCollector {
-	DataCollector() { // default constructor, initialise here
-		NPRESULT result = NPClient_Init(dllPath);
-		if (result == NP_OK) {
-			cout << "TrackIR init success" << "\n";
-		} else {
-			cout << "TrackIR init failed" << "\n";
+	public:
+		DataCollector();
+};
+
+DataCollector::DataCollector(void) { // default constructor, initialise here
+	NPRESULT result = NPClient_Init(dllPath);
+	if (result == NP_OK) {
+		cout << "TrackIR init success" << "\n";
+	} else {
+		cout << "TrackIR init failed" << "\n";
+		if (result == NP_ERR_DLL_NOT_FOUND) {
+			cout << "NP_ERR_DLL_NOT_FOUND" << "\n"; // TODO currently returns this
 		}
 	}
-};
+}
---
+++ GazeTracker.cpp
@@ -0,0 +1,6 @@
+#include "DataCollector.cpp"
+
+int main(int argc, char const *argv[]) {
+	DataCollector trackir;
+	return 0;
+}
