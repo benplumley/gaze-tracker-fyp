600afcdb9c631e2b686da40473ad8a0219d46020
Ben Plumley <benplumley@gmail.com>
2018-01-31 19:42:00 +0000

    added helpers file for matrix operations

---
+++ helpers.cpp
@@ -0,0 +1,65 @@
+#include "eyelike/imgproc.hpp"
+#include "eyelike/opencv2/opencv.hpp"
+
+#define degrees 91.016667f
+
+using namespace cv;
+
+Mat getRotationMatrix();
+Mat getTranslationMatrix();
+Mat getHomography();
+
+// get the homography from headplane to the screen
+Mat getHomography(TRACKIRDATA headframe) {
+	// construct four points in the x-y plane of the head frame
+	vector<Point2f> pts_head;
+	pts_head.push_back(Point2f(141, 131));
+	// construct four points in the x-y plane of the screen frame
+	vector<Point2f> pts_screen;
+
+	Mat h = cv::findHomography(pts_head, pts_screen);
+	return h;
+}
+
+Mat getRotationMatrix(TRACKIRDATA headframe) {
+	double theta_x = headframe.fNPPitch / degrees;
+	double theta_y = headframe.fNPYaw / degrees;
+	double theta_z = headframe.fNPRoll / degrees;
+	// Calculate rotation about x axis
+    Mat R_x = (Mat_<double>(3,3) <<
+               1,       0,              0,
+               0,       cos(theta_x),   -sin(theta_x),
+               0,       sin(theta_x),   cos(theta_x)
+               );
+
+    // Calculate rotation about y axis
+    Mat R_y = (Mat_<double>(3,3) <<
+               cos(theta_y),    0,      sin(theta_y),
+               0,               1,      0,
+               -sin(theta_y),   0,      cos(theta_y)
+               );
+
+    // Calculate rotation about z axis
+    Mat R_z = (Mat_<double>(3,3) <<
+               cos(theta_z),    -sin(theta_z),      0,
+               sin(theta_z),    cos(theta_z),       0,
+               0,               0,                  1);
+
+    // Combined rotation matrix
+    Mat R = R_z * R_y * R_x;
+    return R;
+}
+
+Mat getTranslationMatrix(TRACKIRDATA headframe) {
+	int x = headframe.fNPX;
+	int y = headframe.fNPY;
+	int z = headframe.fNPZ;
+	// to transform a point (a, b, c), multiply (a, b, c, 1) by T
+	Mat T = (Mat_<int>(4,4) <<
+			 1, 0, 0, x,
+			 0, 1, 0, y,
+			 0, 0, 1, z,
+			 0, 0, 0, 1);
+
+	return T;
+}
